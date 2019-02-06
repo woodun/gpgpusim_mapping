1198f39bef5644f93965703edfa80badbe65c0f4
woodun <hwang07@email.wm.edu>
2019-02-06 11:11:16 -0500

    debug

--- src/gpgpu-sim/addrdec.cc
+++ src/gpgpu-sim/addrdec.cc
@@ -209,6 +209,19 @@ void linear_to_raw_address_translation::addrdec_tlx(new_addr_type addr, addrdec_
 			}
 		case CUSTOM5:
 			{
+				std::bitset<64> a(tlx->row);
+				std::bitset<5> chip(tlx->chip);
+				std::bitset<4> b(tlx->bk);
+				chip[0] = a[13]^a[10]^a[9]^a[5]^a[0]^b[3]^b[0]^chip[0];
+				chip[1] = a[12]^a[11]^a[6]^a[1]^b[3]^b[2]^b[1]^chip[1];
+				chip[2] = a[14]^a[9]^a[8]^a[7]^a[2]^b[1]^chip[2];
+				tlx->chip = chip.to_ulong();
+
+				b[0] = a[13]^a[12]^a[11]^a[10]^a[9]^a[6]^a[5]^a[3]^a[0]^b[0];
+				b[1] = a[14]^a[13]^a[12]^a[11]^a[10]^a[7]^a[6]^a[4]^a[1]^b[1];
+				b[2] = a[14]^a[10]^a[9]^a[8]^a[7]^a[6]^a[3]^a[2]^a[0]^b[2];
+				b[3] = a[11]^a[10]^a[9]^a[8]^a[7]^a[4]^a[3]^a[1]^b[3];
+				tlx->bk = b.to_ulong();
 				break;
 			}
 		case CUSTOM6:
